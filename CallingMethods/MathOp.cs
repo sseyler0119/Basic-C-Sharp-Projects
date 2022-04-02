8ecdd12fd1d39c9cbc143b88a29acc0cdbc0aee6
sseyler0119 <sseyler0119@gmail.com>
2022-04-01 17:00:34 -0700

    Create a class with three methods that perform math operations on an integer and return integer values

---
+++ CallingMethods/MathOp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class MathOp
+    {
+        public int addTwo(int num)
+        {
+            int result = num + 2; // add two to passed in number, store in result
+            return result;
+        }
+
+        public int multiplyByFive(int num)
+        {
+            int product = num * 5; // multiply passed in number by 5, store in product
+            return product;
+        }
+
+        public int subtractSeven(int num)
+        {
+            int result = num - 7;
+            return result;
+        }
+
+
+    }
+}
---
+++ CallingMethods/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int result = 0; // store result
+            MathOp m = new MathOp(); // create and instantiate MathOp object
+
+            Console.WriteLine("Enter an integer: ");
+            try 
+            {
+                int number = Convert.ToInt32(Console.ReadLine()); // read in integer from user
+                result = m.addTwo(number); // call addTwo function
+                Console.WriteLine(number + " + 2 = " + result);
+                result = m.multiplyByFive(number); // call multiplyByFive function
+                Console.WriteLine(number + " * 5 = " + result);
+                result = m.subtractSeven(number); // call subractSeven function
+                Console.WriteLine(number + " - 7 = " + result);
+                Console.ReadLine(); // keep console open
+            }
+            catch(FormatException) // incorrect format
+            {
+                Console.WriteLine("Input must be an integer.");
+            }
+            catch(Exception ex) // all other error messages
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine(); // keep console open
+        }
+    }
+}
