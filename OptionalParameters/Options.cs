928e831f60c2935ea78883d775e17051ed05e6d1
sseyler0119 <sseyler0119@gmail.com>
2022-04-02 19:04:09 -0700

    Create method that takes two integers as parameters with one being optional, perform math operation and return result as integer

---
+++ OptionalParameters/Options.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    class Options
+    {
+        public int addTwoNumbers(int num1, int num2 = 0)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+    }
+}
---
+++ OptionalParameters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Options x = new Options(); // create Options object and instantiate
+            int result = 0; // var to store result
+
+            Console.WriteLine("Enter an integer: ");
+            int number = Convert.ToInt32(Console.ReadLine()); // get input as integer
+            Console.WriteLine("Would you like to enter a second number (y/n)?"); // ask if user wants to enter second number
+            char input = Console.ReadLine().ToLower()[0]; // read in character input, convert to lowercase
+            
+            if(input == 'y') // get second number from user
+            {
+                Console.WriteLine("Please enter a second integer: ");
+                int number2 = Convert.ToInt32(Console.ReadLine()); // get input as integer
+                result = x.addTwoNumbers(number, number2); // call addTwoNumbers function, pass in both numbers, store as result
+                Console.WriteLine(number + " + " + number2 + " = " + result); // output both numbers and result to console
+            }
+            else // just pass in first number
+            {
+                result = x.addTwoNumbers(number); // call addTwoNumbers function, pass in number, store as result
+                Console.WriteLine(number + " + 0  = " + result); // output number and result to console
+            }
+            
+            Console.ReadLine(); // keep console open
+        }
+    }
+}
