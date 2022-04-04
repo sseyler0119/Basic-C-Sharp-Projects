c47332c59f35472713cf81de8774ec885a8d2155
sseyler0119 <sseyler0119@gmail.com>
2022-04-04 15:10:17 -0700

    1. Create Person class with FirstName and LastName string properties a) Create void function SayName that outputs person's full name to console 2) Create Employee class that inherits from Person class with integer property Id

---
+++ InheritanceMethodsAndObjects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceMethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" }; // instantiate and initialize Employee object
+            employee.SayName(); // call SayName function on Employee object
+            Console.ReadLine(); // keep console open
+        }
+    }
+}
