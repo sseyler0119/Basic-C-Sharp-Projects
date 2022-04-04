14cdb8d38538e38721169e7895bbd15f50c9d4ea
sseyler0119 <sseyler0119@gmail.com>
2022-04-04 11:05:51 -0700

    Create class with void function that takes in two ints, performs math op on num1, displays num2 to console

---
+++ VoidMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidClass object1 = new VoidClass(); // create class object and instantiate it
+
+            try
+            {
+                Console.WriteLine("Enter an integer: ");
+                int num1 = Convert.ToInt32(Console.ReadLine()); // get input from user as integer
+                Console.WriteLine("Enter another integer: ");
+                int num2 = Convert.ToInt32(Console.ReadLine()); // get input from user as integer
+                object1.twoIntegers(num1, num2); // call twoIntegers function with passed in values
+                object1.twoIntegers(num1: 15, num2: 45); // call twoIntegers function, specify parameters by name
+            }
+            catch (FormatException) // formatting error
+            {
+                Console.WriteLine("Input must be an integer.");
+            }
+            catch(Exception ex) // all other errors
+            {
+                Console.WriteLine(ex.Message); 
+            }
+            
+            Console.ReadLine(); // keep console open
+        }
+    }
+}
---
+++ VoidMethod/VoidClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class VoidClass
+    {
+        // take in two integers as parameters, perform a math operation on first integer, display the second integer to console
+        public void twoIntegers(int num1, int num2)
+        {
+            int result = num1 * 5; // multiple num1 * 5
+            Console.WriteLine("The second number you entered is: " + num2);
+        }
+    }
+}
