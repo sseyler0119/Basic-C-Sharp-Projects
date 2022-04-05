628e49b0a7ba5c232ed0d6a3c74a15472d9c348d
sseyler0119 <sseyler0119@gmail.com>
2022-04-05 13:24:45 -0700

    1. Create IQuittable interface with Quit method 2. Implement Quit method in Employee class 3. Call Quit method from main program with passed in Employee parameters

--- InheritanceMethodsAndObjects/Employee.cs
+++ InheritanceMethodsAndObjects/Employee.cs
@@ -6,13 +6,20 @@ using System.Threading.Tasks;
 
 namespace InheritanceMethodsAndObjects
 {
-    public class Employee : Person // Employee class inherits from abstract Person class
+    public class Employee : Person, IQuittable // Employee class inherits from abstract Person class and IQuittable interface
     {
         public int Id;
 
         public override void SayName() // override abstract method from Person class
         {
             Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+
+        // implement Quit function with passed in Person object parameter
+        public void Quit(Person person)
+        {
+            // employee quits! 
+            Console.WriteLine(FirstName + " " + LastName + " has left the building!");
         }
     }
 }
--- InheritanceMethodsAndObjects/Program.cs
+++ InheritanceMethodsAndObjects/Program.cs
@@ -10,8 +10,18 @@ namespace InheritanceMethodsAndObjects
     {
         static void Main(string[] args)
         {
-            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" }; // instantiate and initialize Employee object
+            Employee employee = new Employee(); // instantiate and initialize Employee object
+
+            // prompt user to enter first and last name
+            Console.WriteLine("Enter first name: ");
+            employee.FirstName = Console.ReadLine(); // read in input, store as FirstName
+            Console.WriteLine("Enter last name: ");
+            employee.LastName = Console.ReadLine(); // read in input, store as LastName
+
             employee.SayName(); // call SayName function on Employee object
+
+            employee.Quit(employee); // call Quit function from IQuittable interface on Employee object
+
             Console.ReadLine(); // keep console open
         }
     }
