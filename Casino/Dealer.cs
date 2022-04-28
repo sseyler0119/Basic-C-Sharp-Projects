04a1dd735c7d1d97e1ee70de77dd03b6308d1b7f
sseyler0119 <sseyler0119@gmail.com>
2022-04-28 10:18:20 -0700

    Casino namespace used with TwentyOne

---
+++ Casino/Dealer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Casino
+{
+    public class Dealer
+    {
+        public string Name { get; set; }
+        public Deck Deck { get; set; } // a dealer HAS-A Deck
+        public int Balance { get; set; }
+
+        public void Deal(List<Card> Hand)
+        {
+            Hand.Add(Deck.Cards.First()); // takes first card in the list and adds it to Hand
+            string card = string.Format(Deck.Cards.First().ToString()); // write the card that was just added to the Hand to 
+            Console.WriteLine(card);
+            StreamWriter file = null;
+            try
+            {
+                using (file = new StreamWriter(@"E:\Logs\log.txt", true))
+                {
+                    file.WriteLine(DateTime.Now);
+                    file.WriteLine(card);
+                }
+            }
+            finally
+            {
+                if (file != null)
+                    file.Dispose();
+            }
+            
+            Deck.Cards.RemoveAt(0); // remove card from top of deck (first in list)
+        }
+    }
+}
---
+++ Casino/IWalkAway.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.Interfaces
+{
+    interface IWalkAway // interface naming conventions start with uppercase "I" to indicate that it's an interface
+    {
+        void WalkAway(Player player); // interfaces are public by default
+    }
+}
